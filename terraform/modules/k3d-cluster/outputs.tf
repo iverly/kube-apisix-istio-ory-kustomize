3f3b9824ea95518c9014d48f3b8d3069a65a356d
iverly <github@iverly.net>
2023-12-25 17:34:55 +0100

    chore(k3d): change output variables name
    Signed-off-by: iverly <github@iverly.net>

--- terraform/modules/k3d-cluster/outputs.tf
+++ terraform/modules/k3d-cluster/outputs.tf
@@ -1,15 +1,15 @@
-output "cluster_endpoint" {
+output "cluster_config_endpoint" {
   value = k3d_cluster.this.credentials[0].host
 }
 
-output "cluster_cluster_ca_certificate" {
+output "cluster_config_cluster_ca_certificate" {
   value = k3d_cluster.this.credentials[0].cluster_ca_certificate
 }
 
-output "cluster_client_certificate" {
+output "cluster_config_client_certificate" {
   value = k3d_cluster.this.credentials[0].client_certificate
 }
 
-output "cluster_client_key" {
+output "cluster_config_client_key" {
   value = k3d_cluster.this.credentials[0].client_key
 }
