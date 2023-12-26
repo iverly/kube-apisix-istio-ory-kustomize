8722b9027897e7a82374604f3ae3c087e08319e7
iverly <github@iverly.net>
2023-12-26 16:00:01 +0100

    fix(terraform): remove vault variabales since we use env
    Signed-off-by: iverly <github@iverly.net>

--- terraform/configuring-vault/main.tf
+++ terraform/configuring-vault/main.tf
@@ -1,14 +1,9 @@
 module "vault-kubernetes-auth" {
   source = "../modules/vault-kubernetes-auth"
-
-  vault_address = var.vault_address
-  vault_token   = var.vault_token
 }
 
 module "vault-pki-linkerd" {
   source = "../modules/vault-pki-linkerd"
 
-  vault_address              = var.vault_address
-  vault_token                = var.vault_token
   vault_kubernetes_auth_path = module.vault-kubernetes-auth.vault_kubernetes_auth_path
 }
