e3c9eef01a2b6cd07b133cea122f3efd0a93409e
iverly <github@iverly.net>
2023-12-28 10:09:15 +0100

    feat(terraform): remove vault pki configuration
    Signed-off-by: iverly <github@iverly.net>

--- terraform/configuring-vault/main.tf
+++ terraform/configuring-vault/main.tf
@@ -1,9 +1,3 @@
 module "vault-kubernetes-auth" {
   source = "../modules/vault-kubernetes-auth"
 }
-
-module "vault-pki-linkerd" {
-  source = "../modules/vault-pki-linkerd"
-
-  vault_kubernetes_auth_path = module.vault-kubernetes-auth.vault_kubernetes_auth_path
-}
--- terraform/modules/vault-pki-linkerd/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vault_kubernetes_auth_path" {
-  description = "Path to the Kubernetes auth backend"
-  type        = string
-  default     = "auth/kubernetes"
-}
